1c20b203ebbda95ee36f988cff6923e61423fe85
SamPratik <pratik.anwar@gmail.com>
2018-03-21 21:49:40 +0600

    removed laravel filemanager package

--- config/app.php
+++ config/app.php
@@ -164,7 +164,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
 
         /*
          * Package Service Providers...
